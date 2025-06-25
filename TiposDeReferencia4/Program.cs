bc353b34b818efdc58d1e784fbd73829a01c56fe
PauloCesar <andradeoliveira2004@icloud.com>
2025-06-25 14:44:45 -0300

    Limpeza geral: aplica .gitignore corretamente

---
+++ TiposDeReferencia4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Definindo vari+aveis do tipo string e object e dynamic");
+
+//Tipo String 
+string nome = "Curso C#";
+String titulo = "Curso C#";
+
+Console.WriteLine(nome);
+Console.WriteLine(titulo);
+
+
+//Tipos Object 
+object nota = 10;
+object nomeObject = "Curso C#";
+object letra = 'A';
+
+Console.WriteLine(nota);
+Console.WriteLine(nomeObject);
+Console.WriteLine(letra);
+
+Console.ReadKey();
